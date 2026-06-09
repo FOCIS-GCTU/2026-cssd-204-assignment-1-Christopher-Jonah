bdea50e02bebca6a858acbc6834af0a1781f2798
Christopher-Jonah <2425403218@live.gctu.edu.gh>
2026-06-09 00:17:49 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,26 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Christopher Nii Amoo Jonah
+# STUDENT ID:  2425403218
+# Email: 2425403218@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Christopher Nii Amoo Jonah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+# Program name: CNAJ
 
-def main():
-  pass #code goes here
+def main();
 
-
-
+print("""
+  ...CCCCCCCCCCCCC......NNNNN........NNNN........AAAAAAAAAA..........JJJJJJJJJJJJ......
+  ...CCCCCCCCCCCCC......NNNNNNN......NNNN.......AAAAAAAAAAAA.........JJJJJJJJJJJJ......
+  ...CCCC...............NNNN.NNN.....NNNN......AAAA.......AAAA...............JJJJ......
+  ...CCCC...............NNNN..NNNN...NNNN......AAAA.......AAAA...............JJJJ......   
+  ...CCCC...............NNNN...NNNN..NNNN......AAAAAAAAAAAAAAA...............JJJJ......  
+  ...CCCC...............NNNN....NNNN.NNNN......AAAA.......AAAA......JJJJ.....JJJJ......
+  ...CCCCCCCCCCCCC..**..NNNN.....NNNNNNNN..**..AAAA.......AAAA..**..JJJJJJJJJJJJJ..**..
+  ...CCCCCCCCCCCCC..**..NNNN........NNNNN..**..AAAA.......AAAA..**..JJJJJJJJJJJJJ..**.. 
+""")
 main()
